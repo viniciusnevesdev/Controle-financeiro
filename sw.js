9a2fcbe8fbfd8cf60f2b4666ad03a4411a86aeb6
Vinicius <viniciusnevez123@gmail.com>
2026-09-11 23:52:44 -0300

    Atualizar ícone oficial do app

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "meu-dinheiro-oficial-1.1.6-env3";
+const CACHE = "meu-dinheiro-oficial-1.1.6-icon-20260912";
 const CACHE_PREFIX = "meu-dinheiro-oficial-";
-const CORE = ["./", "./index.html", "./manifest.webmanifest", "./apple-touch-icon.png", "./assets/index-BILPSsCY.js", "./assets/index-BVjSN_JA.css"];
+const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-official-20260912.jpg", "./assets/index-BILPSsCY.js", "./assets/index-BVjSN_JA.css"];
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
