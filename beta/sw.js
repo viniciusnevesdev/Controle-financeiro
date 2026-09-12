f14a75a975fa3814872bd727aa0dd5becc0d3342
Vinicius <viniciusnevez123@gmail.com>
2026-09-12 00:02:30 -0300

    Atualizar ícones da Beta e do Menu Geral

--- beta/sw.js
+++ beta/sw.js
@@ -1,6 +1,6 @@
-const CACHE = "meu-dinheiro-beta-1.2.0-beta.63-env3";
+const CACHE = "meu-dinheiro-beta-icon-20260912c";
 const CACHE_PREFIX = "meu-dinheiro-beta-";
-const CORE = ["./", "./index.html", "./manifest.webmanifest", "./apple-touch-icon.png", "./assets/index-ko-G7T46.js", "./assets/index-C8MuaHNb.css"];
+const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-beta-20260912c.jpg", "./assets/index-ko-G7T46.js", "./assets/index-C8MuaHNb.css"];
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
