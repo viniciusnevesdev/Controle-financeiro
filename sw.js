a58df3bce28c76480fb132ad683d9eb5b1b561d7
Vinicius <viniciusnevez123@gmail.com>
2026-09-12 00:13:44 -0300

    Corrigir referência do ícone oficial no PWA

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE = "meu-dinheiro-oficial-1.1.8-icon-20260912b";
+const CACHE = "meu-dinheiro-oficial-icon-20260912d";
 const CACHE_PREFIX = "meu-dinheiro-oficial-";
-const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-official-20260912.jpg", "./assets/index-BILPSsCY.js", "./assets/index-BVjSN_JA.css"];
+const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icons/icon-official-20260912d.jpg", "./assets/index-BILPSsCY.js", "./assets/index-BVjSN_JA.css"];
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
